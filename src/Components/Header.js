350e7330779acffccc87013b8daea4ea465fb164
yogeshkumar patil <ybp345@gmail.com>
2023-10-19 06:03:58 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='d-flex justify-content-center align-items-center'>
+        ABCD SHOP
+    </div>
+  )
+}
+
+export default Header
